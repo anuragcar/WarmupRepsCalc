46dcdc69f25ac94c0cf7700fc8a3fed33140ec3b
anuragcar <akar01@nyit.edu>
2023-03-03 16:20:34 -0500

    update lift

--- src/Lift.java
+++ src/Lift.java
@@ -1,5 +1,4 @@
 public abstract class Lift {
-
     int metricWeight, workingSets;
     double weight, firstSet, secondSet, thirdSet, fourthSet, fifthSet;
     boolean highWeight, isMetric;
@@ -8,15 +7,10 @@ public abstract class Lift {
 
     protected abstract boolean highWeight();
     public abstract void setWorkingWeight(int weight, String unit) throws IllegalWeightException;
-
     public abstract void multiplier() throws IllegalWeightException;
     private double checkSetWeight(double setWeight) {
         double roundedWeight = 5 * Math.floor(setWeight / 5);
-        if (roundedWeight < 45) {
-            return 45;
-        }
-        return roundedWeight;
-    }
+        if (roundedWeight < 45) { return 45; }
+        return roundedWeight;   }
     public abstract void printSets(int bar);
-
 }
